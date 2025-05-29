eecf2562350e6d5b591cd9cac8449febe880bfa9
PeterKrupsky <150141900+PeterKrupsky@users.noreply.github.com>
2025-05-29 03:17:27 +0300

    Update urls.py

--- site_app/urls.py
+++ site_app/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -7,9 +7,8 @@ from django.conf.urls.static import static
 app_name = 'site_app'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.first_page),
-    path('page/<int:pk>', views.page, name='page'),
- ]
+    path('', include('site_app.urls')),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
